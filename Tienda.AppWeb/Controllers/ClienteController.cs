40bf5d7f2e8e38d306c0c6a4ee26e5646f85ccd7
Donald Alexander Sullon Porras <dsullon@grupolarepublica.com.pe>
2022-06-02 12:37:04 -0500

    Inicio de Proyecto N-Capas

---
+++ Tienda.AppWeb/Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tienda.Logic;
+
+namespace Tienda.AppWeb.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var listado = ClienteBL.Listar();
+            return View(listado);
+        }
+    }
+}
---
+++ Tienda.Logic/ClienteBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.Core;
+using Tienda.Data;
+
+namespace Tienda.Logic
+{
+    public class ClienteBL
+    {
+        public static List<Cliente> Listar()
+        {
+            using (var db = new TiendaEntities())
+            {
+                return db.Cliente.ToList();
+            }
+        }
+    }
+}
