06564c0ec0b15d66725711eb1bbfea788b79540b
Donald Alexander Sullon Porras <dsullon@grupolarepublica.com.pe>
2022-06-07 10:17:00 -0500

    Creación de la vista de productos

---
+++ Tienda.AppWeb/Controllers/ProductosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tienda.Logic;
+
+namespace Tienda.AppWeb.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var listaProductos = ProductoBL.Listar();
+            return View(listaProductos);
+        }
+    }
+}
---
+++ Tienda.Logic/ProductoBL.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tienda.Core;
+using Tienda.Data;
+
+namespace Tienda.Logic
+{
+    public class ProductoBL
+    {
+        public static List<Producto> Listar()
+        {
+            using (var db = new TiendaEntities())
+            {
+                return db.Producto.ToList();
+            }
+        }
+    }
+}
